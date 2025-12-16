20ebb312f467455b5cf6259cee6061137e4e8eaf
zhou889988 <157271265@qq.com>
2025-12-16 15:50:06 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -143,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // 这里填你需要打包的 H5 页面链接
-        webView.loadUrl("https://h5.xtheta-xau.store");
+        webView.loadUrl("http://in2.goodgame123456.com/");
 
         //显示一些小图片（头像）
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
